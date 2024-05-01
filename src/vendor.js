87f538c26d9a80cf2a7b76eec53841550943bba3
nnda <96333146+nndda@users.noreply.github.com>
2024-05-01 20:28:25 +0800

    Reorder import

--- src/vendor.js
+++ src/vendor.js
@@ -1,3 +1,3 @@
+import "../node_modules/normalize.css/normalize.css";
 import "@fontsource/poppins";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
-import "../node_modules/normalize.css/normalize.css";
