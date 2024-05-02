c6d92446b7c368e9e27b04473f1bb8d1e4cdcec7
Zev <rakha2728@gmail.com>
2024-05-02 18:05:16 +0800

    Add scripts

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import "./styles/theme.css";
 import "./styles/main.css";
 import "./styles/input.css";
 import "./styles/components.css";
+import "./scripts/components.js";
+import "./scripts/api/issues.js";
 import "./scripts/dnd.js";
