b0ab1553a33000351558a024bdf916e822590200
Zev <rakha2728@gmail.com>
2024-05-02 18:05:21 +0800

    Create issues.js

---
+++ src/scripts/api/issues.js
@@ -0,0 +1,5 @@
+// https://api.bitbucket.org/2.0/repositories/{workspace}/{repo_slug}/issues/{issue_id}
+// https://gitlab.example.com/api/v4/projects/{project_id}/issues/{issue_number}
+
+console.log(/https\:\/\/github\.com\/(.+)\/(.+)\/issues\/(\d+)/.exec("https://github.com/mdn/content/issues/33372"));
+// https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}
