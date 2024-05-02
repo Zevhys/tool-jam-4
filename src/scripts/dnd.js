9e723c695d6080781d17be975e4934bbee9fa527
Zev <rakha2728@gmail.com>
2024-05-02 18:05:37 +0800

    Disable script

--- src/scripts/dnd.js
+++ src/scripts/dnd.js
@@ -1,47 +1,47 @@
-import "@interactjs/auto-start";
-import "@interactjs/actions/drag";
-import "@interactjs/actions/resize";
-import "@interactjs/modifiers";
-import "@interactjs/dev-tools";
-import interact from "@interactjs/interact";
+// import "@interactjs/auto-start";
+// import "@interactjs/actions/drag";
+// import "@interactjs/actions/resize";
+// import "@interactjs/modifiers";
+// import "@interactjs/dev-tools";
+// import interact from "@interactjs/interact";
 
-const position = { x: 0, y: 0 };
+// const position = { x: 0, y: 0 };
 
-interact(".kanban-item").draggable({
-  listeners: {
-    start(event) {
-      console.log(event.type, event.target);
-    },
-    move(event) {
-      position.x += event.dx;
-      position.y += event.dy;
+// interact(".kanban-item").draggable({
+//   listeners: {
+//     start(event) {
+//       console.log(event.type, event.target);
+//     },
+//     move(event) {
+//       position.x += event.dx;
+//       position.y += event.dy;
 
-      event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
-    },
-  },
-  modifiers: [
-    interact.modifiers.restrictRect({
-      restriction: "parent",
-    }),
-  ],
-});
+//       event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
+//     },
+//   },
+//   modifiers: [
+//     // interact.modifiers.restrictRect({
+//     //   restriction: "parent",
+//     // }),
+//   ],
+// });
 
-interact(".kanban-item").resizable({
-  edges: { top: true, left: true, bottom: true, right: true },
-  listeners: {
-    move: function (event) {
-      let { x, y } = event.target.dataset;
+// interact(".kanban-item").resizable({
+//   edges: { top: true, left: true, bottom: true, right: true },
+//   listeners: {
+//     move: function (event) {
+//       let { x, y } = event.target.dataset;
 
-      x = (parseFloat(x) || 0) + event.deltaRect.left;
-      y = (parseFloat(y) || 0) + event.deltaRect.top;
+//       x = (parseFloat(x) || 0) + event.deltaRect.left;
+//       y = (parseFloat(y) || 0) + event.deltaRect.top;
 
-      Object.assign(event.target.style, {
-        width: `${event.rect.width}px`,
-        height: `${event.rect.height}px`,
-        transform: `translate(${x}px, ${y}px)`,
-      });
+//       Object.assign(event.target.style, {
+//         width: `${event.rect.width}px`,
+//         height: `${event.rect.height}px`,
+//         transform: `translate(${x}px, ${y}px)`,
+//       });
 
-      Object.assign(event.target.dataset, { x, y });
-    },
-  },
-});
+//       Object.assign(event.target.dataset, { x, y });
+//     },
+//   },
+// });
