1f4739381c5f74527cd95c3663ab244adf4a8138
Zev <rakha2728@gmail.com>
2024-05-03 21:44:56 +0800

    Prototype task lists

--- src/scripts/components.js
+++ src/scripts/components.js
@@ -22,3 +22,21 @@ const d = document;
 d.querySelector(".create-item-note").addEventListener("click", (e) => {
   e.target.parentElement.querySelector(".kanban-item-container").innerHTML += kanbanItemTemplate;
 });
+
+// TODO
+//   <form id="form">
+//   </form>
+//   <button id="btn">tambah</button>
+//   <input type="text" name="" id="inp"></input>
+
+// let form = document.querySelector("#form");
+
+// document.querySelector("#btn").addEventListener("click", () => {
+//   let input = document.querySelector("#inp").value;
+//   const template = `
+//         <input type="checkbox" name="cb" id="${input.replace(" ", "-")}">
+//         <label for="${input.replace(" ", "-")}">${input}</label>
+//       `;
+//   // console.log(input)
+//   form.innerHTML += template;
+// });
