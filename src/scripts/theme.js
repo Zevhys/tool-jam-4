c7c4122ff6e325e6d917df4fe49469c904968bc8
nnda <96333146+nndda@users.noreply.github.com>
2024-05-13 02:10:34 +0800

    Implement persistent theme

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,4 +1,12 @@
 import $ from "jquery";
+
+$(window).on("load", () => {
+  if (localStorage.getItem("setTheme") != null) {
+    $("body").toggleClass("light-mode", localStorage.getItem("setTheme") === "light" ? true : false);
+  }
+});
+
 $("#toggle-theme").bind("click", () => {
   $("body").toggleClass("light-mode");
+  localStorage.setItem("setTheme", $("body").hasClass("light-mode") ? "light" : "dark");
 });
